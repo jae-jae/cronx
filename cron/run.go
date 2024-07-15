a12fde7acff2da724925e491080f8c87e5b2b4c8
Jaeger <jaegerCode@gmail.com>
2024-07-15 11:30:33 +0800

    feat: log config

--- cron/run.go
+++ cron/run.go
@@ -1,9 +1,24 @@
 package cron
 
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
 func Run() {
+	initLog()
+
 	cmd := NewCMD()
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initLog() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetLevel(log.InfoLevel)
+	log.SetOutput(os.Stdout)
 }
